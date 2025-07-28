571a400bf378a2a8e6ffa41548a7c8a07b2a05c8
Ironraptor3 <dannyribaudo@gmail.com>
2025-07-28 02:36:51 -0400

    Checkpoint 7/28; Needs work

--- bin/generate_card.py
+++ bin/generate_card.py
@@ -1,8 +1,13 @@
+import argparse
 import hashlib
 import json
 import os
+import sys
 
+import PIL
 from psd_tools import PSDImage
+from psd_tools.api.layers import PixelLayer
+from psd_tools.compression import Compression
 
 # Constants
 COLOR_TITLE = 0x000000
@@ -15,25 +20,38 @@ TITLE_TEXT_FONT = 'P22 Johnston Underground Regular'
 TITLE_TEXT_SIZE = 40
 DESC_TEXT_FONT = 'NotoSansDisplay-SemiCondensed'
 DESC_TEXT_SIZE = 32
+COST_TEXT_FONT = 'P22 Johnston Underground Regular'
+COST_TEXT_SIZE = 146
 
+PSD_NAME = 'lor_template.psd'
 PSD_MD5 = '53ed4a18dbd596add12463898e6a95bd'
 
 def init_data(parent_dir, file_path):
     path = os.path.join(parent_dir, file_path)
-    data = json.load(path)
+    with open(path, 'r') as path_fd:
+        data = json.load(path_fd)
     data['dir'] = os.path.dirname(path)
     return data
 
-def get_field(data, field, load_files=True):
+def get_field(data, field, relative=False):
     if field in data:
+        if relative:
+            return os.path.join(data['dir'], data[field])
         return data[field]
+
     parent_data = data.get('parent_data', None)
     if parent_data is None:
         parent = data.get('parent', None)
         if parent is not None:
-            parent_data = init_data(data['dir'], data['parent'])
+            parent_data = init_data(data['dir'], parent)
+
     if parent_data is not None:
-        field_data = get_field(parent_data, field, load_files)
+        field_data = get_field(parent_data, field, relative)
+        if field_data is not None and relative:
+            field_data = os.path.join(os.path.dirname(data['parent']), field_data)
+        data[field] = field_data
+        return field_data
+
     return None
 
 def get_layer(current_layer, name, partial=False):
@@ -46,24 +64,23 @@ def get_layer(current_layer, name, partial=False):
     return None
 
 def edit_dice_number(dice_number, data):
-    num_dice = len(data['dice'])
+    num_dice = len(get_field(data, 'dice'))
 
     dn = None
     found = False
     for dn in dice_number:
-        if num_dice == int(dn.name.split()[0])
+        if num_dice == int(dn.name.split()[0]):
             found = True
             break
 
-    for i in num_dice:
+    for i in range(num_dice):
         dice_layer_i = get_layer(dn, str(i + 1), partial=True) # 1-based indexing in psd
         for dice_type in dice_layer_i:
-            dice_type.visible = dice_type.name.lower() == data['dice'][i]['type'].lower()
-
+            dice_type.visible = dice_type.name.lower() == get_field(data, 'dice')[i]['type'].lower()
 
 def edit_page_type(page_type, data):
     for t in page_type:
-        t.visible = data['type'].lower() == t.name.lower()
+        t.visible = get_field(data, 'type').lower() == t.name.lower()
 
 def edit_page_cost(page_cost, data):
     # Make these invisible; handle later
@@ -72,7 +89,7 @@ def edit_page_cost(page_cost, data):
 
 def edit_page_rarity(page_rarity, data):
     for rarity in page_rarity:
-        if data['rarity'] == rarity.name:
+        if get_field(data, 'rarity') == rarity.name:
             rarity.visible = True
             edit_page_type(get_layer(page_rarity, 'Card Type'), data)
             edit_page_cost(get_layer(page_rarity, 'Irrelevant Stuff'), data)
@@ -80,15 +97,26 @@ def edit_page_rarity(page_rarity, data):
             rarity.visible = False
 
 def edit_page_base(page_base, data):
-    get_layer(page_base, 'Do Not Delete').visible = data['grit']
-    get_layer(page_base, '176m2').visible = False # Remove default image
-    # TODO
+    get_layer(page_base, 'Do Not Delete').visible = get_field(data, 'grit') # This is the grit
+    sample_img_layer = get_layer(page_base, '176m2')
+    sample_img_layer.visible = False # Remove default image
+    bbox = sample_img_layer.bbox
+
+    with PIL.Image.open(get_field(data, 'art', relative=True)) as art:
+        page_art = PixelLayer.frompil(
+            pil_im=art,
+            #psd_file=None,
+            top=bbox[1], # Top
+            left=bbox[0], # Left
+            compression=Compression.RLE
+        )
+        page_base.append(page_art)
 
 def edit_combat_page(combat_page, data):
     # No need for notes
     get_layer(combat_page, 'Notes', partial=True).visible = False
     # Doing this ourselves in PIL
-    get_layer(combat_page, 'Card Text & Dice Detail').visible = False
+    get_layer(combat_page, 'Card Text & Dice Details').visible = False
 
     edit_dice_number(get_layer(combat_page, 'Number of Dice'), data)
     edit_page_rarity(get_layer(combat_page, 'Card Rarity'), data)
@@ -102,15 +130,39 @@ def edit_page_class(psd, data):
     combat_page.visible = True
     edit_combat_page(combat_page, data)
 
-# TODO Need to use layer.visible = True | False + compose(force=True)
-def main(psd_path, json_path)
+def main(data_path, output_path, asset_path):
+    psd_path = os.path.join(asset_path, PSD_NAME)
+    with open(psd_path, 'rb') as check_psd:
+        check_md5 = hashlib.file_digest(check_psd, 'md5').hexdigest()
+        assert check_md5 == PSD_MD5, 'MD5 sum for %s did not match expected (%s!=%s), wrong psd supplied!' % (psd_path, check_md5, PSD_MD5)
     psd = PSDImage.open(psd_path)
-    data = init_data(json_path)
+    data = init_data('', data_path)
 
     edit_page_class(psd, data)
+    # Force redraws the psd instead of grabbing from the cache / buf
+    img = psd.composite(force=True)
+
+    # TODO text
+
+    img.save(output_path, format='PNG') # Finally, output to png
 
 def get_args():
-    pass # TODO
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_path', type=str, help='Path to data to create a card')
+    parser.add_argument('output_path', type=str, help='Path to output (ought to be a png file)')
+    parser.add_argument('-a', '--asset-path', type=str, default=None,
+            help='Path to the assets folder. Defaults to ../assets (relative to this script)')
+    args = parser.parse_args()
+
+    # As stated in the help text, `<scriptdir>/../assets/`
+    if args.asset_path is None:
+        args.asset_path = os.path.join(
+            os.path.dirname(sys.argv[0]),
+            '..',
+            'assets')
+
+    return (args.data_path, args.output_path, args.asset_path)
 
 if __name__ == '__main__':
     main(*get_args())
+
